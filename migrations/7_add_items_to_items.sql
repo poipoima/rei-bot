ab5103a4f48fe98347e9d583d0ae864c1668d1b3
alex <alex.suspicious@gmail.com>
2025-10-02 15:12:31 +0500

    standart items

---
+++ migrations/7_add_items_to_items.sql
@@ -0,0 +1,7 @@
+INSERT INTO items (icon, name, description) 
+VALUES 
+    (":rock:", "rock", "regular rock"),
+    (":bone:", "bone", "a bone?"),
+    (":potato:", "potato", "yummy potato"),
+    (":cake:", "cake", "ooh looks yummy"),
+    (":gift:", "gift", "what may be inside?");
