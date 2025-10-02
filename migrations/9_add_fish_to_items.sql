793739594b262ec0df69b68f3209c99b73100090
alex <alex.suspicious@gmail.com>
2025-10-02 15:49:09 +0500

    New items

---
+++ migrations/9_add_fish_to_items.sql
@@ -0,0 +1,6 @@
+INSERT INTO items (icon, name, description) 
+VALUES 
+    (":fish:", "fish", "regular fish"),
+    (":tropical_fish:", "tropical fish", "it's a tropical fish"),
+    (":shrimp:", "shrimp", "oh, a shrimp!"),
+    (":blowfish:", "blowfish", "regular blowfish");
